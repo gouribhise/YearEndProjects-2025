cd5b22c59c71aa6513ff643c74de43ece76cbea3
gouribhise <gouri.a.bhise@gmail.com>
2025-11-02 12:42:03 +0530

    added player and dealer cards

--- JavaScript/js/advBlackJack.js
+++ JavaScript/js/advBlackJack.js
@@ -1,7 +1,16 @@
-var deck;
+let dealerSum = 0;
+let playerSum = 0;
+
+let dealerAceCount = 0;
+let playerAceCount = 0; 
+
+let hidden;
+let deck;
+ 
 window.onload=function(){
     buildDeck();
     shuffleDeck();
+    startGame();
 }
 
 function buildDeck(){
@@ -24,6 +33,53 @@ function buildDeck(){
         deck[j]=temp;
     }
     console.log(deck)
+ }
+
+ function startGame(){
+    hidden=deck.pop();
+    dealerSum+=getValue(hidden)
+    dealerAceCount+=checkAce(hidden)
+    console.log('hidden',hidden)
+    console.log(dealerSum)
+
+    while(dealerSum<17){
+        let cardImg=document.createElement("img");
+        let card=deck.pop()
+        cardImg.src="./cards/"+card+".png";
+        dealerSum+=getValue(card);
+        dealerAceCount+=checkAce(card);
+        document.getElementById("dealer_cards").append(cardImg)
+    }
+    console.log(dealerSum)
+
+    for(let i=0;i<2;i++){
+        let cardImg=document.createElement("img")
+        let card=deck.pop()
+        cardImg.src="./cards/"+card+".png";
+        playerSum+=getValue(card)
+        playerAceCount+=checkAce(card)
+        document.getElementById("player_cards").append(cardImg)
+    }
+ }
+
+ function getValue(card){
+    let data=card.split("-")
+    let value=data[0]
+
+    if(isNaN(value)){
+        if(value=="A"){
+            return 11
+        }return 10
+    }
+    return parseInt(value)
+
+ }
+
+ function checkAce(card){
+    if (card[0]=='A'){
+        return 1
+    }
+    return 0
  }
 var hit_btn=document.getElementById('hit')
 var stay_btn=document.getElementById('stay')
